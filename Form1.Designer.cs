7a760aff61b5756a40abdec0ff924f68ad46dff5
CuongSu <73159675+cuongsuhuyy@users.noreply.github.com>
2021-10-23 16:35:45 +0700

    Create project

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AutoCreateEmailWIthoutNumberPhone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Run
+            // 
+            this.btn_Run.Location = new System.Drawing.Point(174, 156);
+            this.btn_Run.Name = "btn_Run";
+            this.btn_Run.Size = new System.Drawing.Size(109, 42);
+            this.btn_Run.TabIndex = 0;
+            this.btn_Run.Text = "Run";
+            this.btn_Run.UseVisualStyleBackColor = true;
+            this.btn_Run.Click += new System.EventHandler(this.btn_Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Run;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCreateEmailWIthoutNumberPhone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Run_Click(object sender, EventArgs e)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument(@"user-data-dir=C:\Users\Shishi\AppData\Local\Google\Chrome\User Data\");
+            Thread.Sleep(5000);
+
+            ChromeDriver chromeDriver = new ChromeDriver(options);
+            chromeDriver.Url = "https://accounts.google.com/signup/v2/webcreateaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=SignUp";
+            chromeDriver.Navigate();
+        }
+    }
+}
